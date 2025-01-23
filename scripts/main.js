aaf701f51c2da194f957ea1f45f7af571db7224a
Huniity <adrien.dejonc@gmail.com>
2025-01-23 15:25:05 +0000

    updating for final version

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 async function loadNavbar() {
-    const response = await fetch("../components/navbar.html");
+    const response = await fetch("./components/navbar.html");
     const navbarHtml = await response.text();
     document.getElementById("navbar").innerHTML = navbarHtml;
 }
