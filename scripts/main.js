269d6761433e7d7e0b882bd874b7dc4ee75584aa
Huniity <adrien.dejonc@gmail.com>
2025-01-23 17:07:09 +0000

    updating to last version

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,36 @@
 async function loadNavbar() {
-    const response = await fetch("./components/navbar.html");
+    const response = await fetch("../components/navbar.html");
     const navbarHtml = await response.text();
     document.getElementById("navbar").innerHTML = navbarHtml;
 }
 
+async function loadFooter() {
+    const response = await fetch("../components/footer.html");
+    const footerHtml = await response.text();
+    document.getElementById("footer").innerHTML = footerHtml;
+  }
+
 loadNavbar();
+loadFooter();
+
+
+function checkDarkMode() {
+    const hasDarkMode = window.localStorage.getItem("darkMode");
+    if (hasDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+  
+  checkDarkMode();
+  
+  function toggleDarkMode() {
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.remove("dark");
+      window.localStorage.removeItem("darkMode");
+    } else {
+      document.body.classList.add("dark");
+      window.localStorage.setItem("darkMode", "true");
+    }
+  }
