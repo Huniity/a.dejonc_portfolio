12d29a29c4608930fafad69fa922a60edcb0ae6c
Adrien Dejonc <adrien.dejonc@gmail.com>
2025-01-23 20:57:46 +0000

    update for last version

--- scripts/error.js
+++ scripts/error.js
@@ -1,33 +1,42 @@
+const formulario = document.getElementById("my_form");
+formulario.addEventListener("submit", (evento) => {
 
-    const formulario = document.getElementById("my_form");
-    const erro = document.getElementById("erro");
-
-
-    formulario.addEventListener("submit", (evento) => {
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
 
 
-        if (!name.trim()) {
-            evento.preventDefault();
-            erro.innerText = "O campo 'Nome' é obrigatório.";
-            document.getElementById("name").classList.add("erro");
-        } else if (!email.includes("@")) {
-            evento.preventDefault();
-            erro.innerText = "Introduza um e-mail válido.";
-            document.getElementById("email").classList.add("erro");
-        } else if (message.trim()) {
-            evento.preventDefault();
-            erro.innerText = "Introduza uma mensagem.";
-            document.getElementById("message").classList.add("erro");
-        } else {
-            erro.innerText = "";
-            document.getElementById("name").classList.remove("erro");
-            document.getElementById("email").classList.remove("erro");
-            document.getElementById("message").classList.remove("erro");
-        }
-    });
+    document.getElementById("erro-name").innerText = "";
+    document.getElementById("erro-email").innerText = "";
+    document.getElementById("erro-message").innerText = "";
+    
+
+    document.getElementById("name").classList.remove("erro");
+    document.getElementById("email").classList.remove("erro");
+    document.getElementById("message").classList.remove("erro");
 
 
+    let formIsValid = true;
 
+
+    if (!name.trim()) {
+        evento.preventDefault();
+        document.getElementById("erro-name").innerText = "O campo 'Nome' é obrigatório.";
+        document.getElementById("name").classList.add("error");
+        formIsValid = false;
+    }
+
+    if (!email.includes("@")) {
+        evento.preventDefault();
+        document.getElementById("erro-email").innerText = "Introduza um e-mail válido.";
+        document.getElementById("email").classList.add("error");
+        formIsValid = false;
+    }
+
+    if (!message.trim()) {
+        evento.preventDefault();
+        document.getElementById("erro-message").innerText = "Introduza uma mensagem.";
+        document.getElementById("message").classList.add("error");
+        formIsValid = false;
+    }
+});
