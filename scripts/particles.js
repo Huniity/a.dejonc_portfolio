6dcfe17372f05f73e74ffbd98e1dd604a9497598
Huniity <adrien.dejonc@gmail.com>
2025-01-23 23:36:15 +0000

    updating to last version

--- scripts/particles.js
+++ scripts/particles.js
@@ -77,8 +77,8 @@ function start() {
         ],
         starsAngle = 145,
         shootingStarSpeed = {
-            min: 15,
-            max: 20
+            min: 5,
+            max: 15
         },
         shootingStarOpacityDelta = 0.01,
         trailLengthDelta = 0.01,
@@ -201,41 +201,80 @@ function start() {
             currentTrailLength = (maxTrailLength * p.trailLengthDelta),
             pos = lineToAngle(x, y, -currentTrailLength, p.getHeading());
 
-            
-        context.fillStyle = "rgba(255, 255, 255, " + p.opacity + ")";
-        // context.beginPath();
-        // context.arc(x, y, p.radius, 0, Math.PI * 2, false);
-        // context.fill();
-        var starLength = 5;
-        context.beginPath();
-        context.moveTo(x - 1, y + 1);
+            const hasDarkMode = window.localStorage.getItem("darkMode");
+            if (hasDarkMode) {
+                context.fillStyle = "#000000";
+                // context.beginPath();
+                // context.arc(x, y, p.radius, 0, Math.PI * 2, false);
+                // context.fill();
+                var starLength = 5;
+                context.beginPath();
+                context.moveTo(x - 1, y + 1);
+        
+                context.lineTo(x, y + starLength);
+                context.lineTo(x + 1, y + 1);
+        
+                context.lineTo(x + starLength, y);
+                context.lineTo(x + 1, y - 1);
+        
+                context.lineTo(x, y + 1);
+                context.lineTo(x, y - starLength);
+        
+                context.lineTo(x - 1, y - 1);
+                context.lineTo(x - starLength, y);
+        
+                context.lineTo(x - 1, y + 1);
+                context.lineTo(x - starLength, y);
+        
+                context.closePath();
+                context.fill();
+        
+                //trail
+                context.fillStyle = "#000000";
+                context.beginPath();
+                context.moveTo(x - 1, y - 1);
+                context.lineTo(pos.x, pos.y);
+                context.lineTo(x + 1, y + 1);
+                context.closePath();
+                context.fill();
+            } 
+            else {
+                context.fillStyle = "rgba(255, 255, 255, " + p.opacity + ")";
+                // context.beginPath();
+                // context.arc(x, y, p.radius, 0, Math.PI * 2, false);
+                // context.fill();
+                var starLength = 5;
+                context.beginPath();
+                context.moveTo(x - 1, y + 1);
+        
+                context.lineTo(x, y + starLength);
+                context.lineTo(x + 1, y + 1);
+        
+                context.lineTo(x + starLength, y);
+                context.lineTo(x + 1, y - 1);
+        
+                context.lineTo(x, y + 1);
+                context.lineTo(x, y - starLength);
+        
+                context.lineTo(x - 1, y - 1);
+                context.lineTo(x - starLength, y);
+        
+                context.lineTo(x - 1, y + 1);
+                context.lineTo(x - starLength, y);
+        
+                context.closePath();
+                context.fill();
+        
+                //trail
+                context.fillStyle = "rgba(255, 221, 157, " + p.opacity + ")";
+                context.beginPath();
+                context.moveTo(x - 1, y - 1);
+                context.lineTo(pos.x, pos.y);
+                context.lineTo(x + 1, y + 1);
+                context.closePath();
+                context.fill();
+        }
 
-        context.lineTo(x, y + starLength);
-        context.lineTo(x + 1, y + 1);
-
-        context.lineTo(x + starLength, y);
-        context.lineTo(x + 1, y - 1);
-
-        context.lineTo(x, y + 1);
-        context.lineTo(x, y - starLength);
-
-        context.lineTo(x - 1, y - 1);
-        context.lineTo(x - starLength, y);
-
-        context.lineTo(x - 1, y + 1);
-        context.lineTo(x - starLength, y);
-
-        context.closePath();
-        context.fill();
-
-        //trail
-        context.fillStyle = "rgba(255, 221, 157, " + p.opacity + ")";
-        context.beginPath();
-        context.moveTo(x - 1, y - 1);
-        context.lineTo(pos.x, pos.y);
-        context.lineTo(x + 1, y + 1);
-        context.closePath();
-        context.fill();
     }
 
     //Run
