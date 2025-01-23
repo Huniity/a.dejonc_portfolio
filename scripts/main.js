76166548f82546db74219c4d557c60f5343e84fd
Huniity <adrien.dejonc@gmail.com>
2025-01-23 17:46:01 +0000

    updating to last version

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 async function loadNavbar() {
-    const response = await fetch("../components/navbar.html");
+    const response = await fetch("./components/navbar.html");
     const navbarHtml = await response.text();
     document.getElementById("navbar").innerHTML = navbarHtml;
 }
 
 async function loadFooter() {
-    const response = await fetch("../components/footer.html");
+    const response = await fetch("./components/footer.html");
     const footerHtml = await response.text();
     document.getElementById("footer").innerHTML = footerHtml;
   }
